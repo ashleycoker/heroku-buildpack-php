ff720dfda648d40800719e063f18bfcfc8b2ae28
ashleycoker <ashley.coker@prowebsoftware.net>
2014-10-20 13:54:57 +0100

    Update heroku.conf.php

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -307,6 +307,9 @@ http {
             # try_files resets $fastcgi_path_info, see http://trac.nginx.org/nginx/ticket/321, so we use the if instead
             fastcgi_param PATH_INFO $fastcgi_path_info if_not_empty;
             
+            # try to serve file directly, fallback to rewrite
+            try_files $uri @rewriteapp;
+            
             if (!-f $document_root$fastcgi_script_name) {
                 # check if the script exists
                 # otherwise, /foo.jpg/bar.php would get passed to FPM, which wouldn't run it as it's not in the list of allowed extensions, but this check is a good idea anyway, just in case

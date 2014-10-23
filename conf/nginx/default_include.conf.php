db93f23cd3d85fcec59f3aca00b4b807e6781d75
ashleycoker <ashley.coker@prowebsoftware.net>
2014-10-23 15:41:43 +0100

    No more index.php

--- conf/nginx/default_include.conf.php
+++ conf/nginx/default_include.conf.php
@@ -1,5 +1,10 @@
 location / {
     index  index.php index.html index.htm;
+    try_files $uri $uri/ @rewrites;
+}
+
+location @rewrites {
+    rewrite ^ /index.php last;
 }
 
 # for people with app root as doc root, restrict access to a few things
